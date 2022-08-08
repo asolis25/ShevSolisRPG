487c9868c1f9ba3928bea335744e930ffab73863
asolis25 <asolis@macalester.edu>
2022-08-08 06:13:18 -0500

    Movement Animation Basics
    Basic integrated directional movement animations.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
-
+    public Animator animator;
     private Vector2 movement;
 
     // Update is called once per frame
@@ -21,6 +21,10 @@ public class PlayerMovement : MonoBehaviour
 
         // Combined normalized movement vector
         movement = new Vector2(movement.x, movement.y).normalized;
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     private void FixedUpdate()
